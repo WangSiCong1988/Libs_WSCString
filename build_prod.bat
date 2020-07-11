dc4f527c40d0823fbe29ae7ce589462d6140b2f1
WangSiCong1988 <wsc_pandaTV@protonmail.com>
2020-07-12 00:13:45 +1000

    modify README.md

---
+++ build_prod.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+rd /s /q ".\prod"
+md ".\prod"
+xcopy /y /d ".\WinString_DLL\Release\WinString_DLL.lib" ".\prod"
+xcopy /y /d ".\WinString_DLL\Release\WinString_DLL.dll" ".\prod"
+xcopy /y /d ".\WinString_DLL\WinString_DLL.h" ".\prod"
