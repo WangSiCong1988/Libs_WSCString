c70d2d071e826c38cab11a81d239088ceaa03a9d
WangSiCong1988 <wsc_pandaTV@protonmail.com>
2020-07-12 21:50:09 +1000

    README.md 图片改成base64保存

--- build_prod.bat
+++ build_prod.bat
@@ -1,11 +1,10 @@
 ﻿@echo off
 rd /s /q ".\prod"
 md ".\prod"
-md ".\prod\img"
+
 xcopy /y /d ".\.gitignore" ".\prod"
 xcopy /y /d ".\LICENSE" ".\prod"
 xcopy /y /d ".\README.md" ".\prod"
-xcopy /y /d ".\img\*" ".\prod\img"
 
 xcopy /y /d ".\WinString_DLL\Release\WinString_DLL.lib" ".\prod"
 xcopy /y /d ".\WinString_DLL\Release\WinString_DLL.dll" ".\prod"
