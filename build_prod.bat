e163c65bed06514bfb82977c6bbc79738090d58f
WangSiCong1988 <wsc_pandaTV@protonmail.com>
2020-07-12 21:57:10 +1000

    生产脚本不再增加LICENSE, READMED.md, .gitignore

--- build_prod.bat
+++ build_prod.bat
@@ -2,10 +2,6 @@
 rd /s /q ".\prod"
 md ".\prod"
 
-xcopy /y /d ".\.gitignore" ".\prod"
-xcopy /y /d ".\LICENSE" ".\prod"
-xcopy /y /d ".\README.md" ".\prod"
-
 xcopy /y /d ".\WinString_DLL\Release\WinString_DLL.lib" ".\prod"
 xcopy /y /d ".\WinString_DLL\Release\WinString_DLL.dll" ".\prod"
 xcopy /y /d ".\WinString_DLL\WinString_DLL.h" ".\prod"
